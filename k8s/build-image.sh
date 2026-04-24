4f2132b485df3c860d69e971d0cc98add39d3988
ahmed kabil <ahmedskabil@gmail.com>
2026-04-25 01:21:51 +0300

    update the naming remove the _ and add -

--- k8s/build-image.sh
+++ k8s/build-image.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker build -t hospital-metric_servo-image:latest ../backend/5-metric_servo_service/
-docker save hospital-metric_servo-image:latest > metric_servo-image.tar
+docker build -t ahmedkabil/hospital-metric-servo-service:latest ../5-metric_servo_service/
+docker save ahmedkabil/hospital-metric-servo-service:latest > metric_servo-image.tar
 sudo ctr -n k8s.io images import metric_servo-image.tar
 rm -rf metric_servo-image.tar
